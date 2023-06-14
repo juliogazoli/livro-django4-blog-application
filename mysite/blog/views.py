ff366656dcf83b24d29c94e1282f6a9c062e1418
Julio Gazoli <julio_gazoli@hotmail.com>
2023-06-14 18:50:36 -0300

    Revert "class-based view"
    This reverts commit b318e8445f27b31a5c50629e2b64f05459c4c8fe.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,3 @@
-from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -32,13 +31,3 @@ def post_detail(request, year, month, day, post):
         publish__day=day
     )
     return render(request, 'blog/post/detail.html', {'post': post})
-
-
-class PostListView(ListView):
-    """
-    Alternative post list view
-    """
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
