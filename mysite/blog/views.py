9a188827790148abdccf936fa41fe285433c1aef
Julio Gazoli <julio_gazoli@hotmail.com>
2023-06-14 18:50:58 -0300

    sending emails with forms

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,12 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+from django.core.mail import send_mail
+from .forms import EmailPostForm
+from django.views.generic import ListView
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -31,3 +40,41 @@ def post_detail(request, year, month, day, post):
         publish__day=day
     )
     return render(request, 'blog/post/detail.html', {'post': post})
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'julio.gazoli@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
