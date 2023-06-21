3ef4579d20a2e7de199ff62bcff5060298f92776
Julio Gazoli <julio_gazoli@hotmail.com>
2023-06-21 17:31:04 -0300

    Tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -37,11 +38,19 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+
     # List of active comments for this post
     comments = post.comments.filter(active=True)
+
     # Form for users to comment
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
