87126fef2b94e5c89fdb820e6a412c24a42d2551
Julio Gazoli <julio_gazoli@hotmail.com>
2023-06-28 17:19:47 -0300

    stemming and ranking results

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.core.mail import send_mail
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
@@ -113,7 +113,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body', config='portuguese')
+            search_query = SearchQuery(query, config='portuguese')
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
 
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
