01d36bd32d4d6e6e5375ffd69111bda10b5feb91
Julio Gazoli <julio_gazoli@hotmail.com>
2023-06-28 19:38:46 -0300

    searching with trigram similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.core.mail import send_mail
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
@@ -113,11 +113,8 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
-            search_query = SearchQuery(query, config='portuguese')
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by('-rank')
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gte=0.1).order_by('-similarity')
 
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
